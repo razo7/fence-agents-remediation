fee048a1907c811a48203e74cf8d3c3b8baa2de3
razo7 <oraz@redhat.com>
2023-05-09 09:44:21 +0300

    test 3

--- test/e2e/far_e2e_test.go
+++ test/e2e/far_e2e_test.go
@@ -27,7 +27,7 @@ const (
 	fenceAgentAction         = "reboot"
 	nodeIdentifierPrefixAWS  = "--plug"
 	nodeIdentifierPrefixIPMI = "--ipport"
-	nodeIndex                = 0
+	nodeIndex                = 3
 	succeesStatusMessage     = "\"Status: ON\n\""
 	succeesRebootMessage     = "\"Success: Rebooted\""
 	containerName            = "manager"
@@ -266,7 +266,7 @@ func getNodeBootTime(nodeName string) (time.Time, error) {
 
 // buildExpectedLogOutput
 func buildExpectedLogOutput(clusterPlatformType configv1.PlatformType, nodeIdentifierPrefix, testNodeID string) string {
-	// Example -> "--plug=i-04172bb40a6a83804"], "stdout": "Status: ON\n" OR  "--ipport=6230"], "stdout": "Success: Rebooted\n"
+	// Example -> "--plug=i-04172bb40a6a83804"], "stdout": "Status: ON\n" ORoc   "--ipport=6230"], "stdout": "Success: Rebooted\n"
 	var expectedString string
 	if fenceAgentAction == "status" {
 		expectedString = succeesStatusMessage
